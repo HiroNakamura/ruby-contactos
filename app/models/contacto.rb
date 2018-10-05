28dd79147d6f0758d2c29ecb88c857f93dcb181f
HIRONAKAMURA <carraro.ariel@gmail.com>
2018-10-05 11:25:09 -0500

    Cambios

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -1,3 +1,13 @@
 class Contacto < ApplicationRecord
-    
+    #id :primary_key 
+    #nombre :string
+    #apellidos :string
+    #telefono :string
+    #correo :string
+    #direccion :string
+    #fecha :datetime
+
+    def self.down
+        drop_table :contacto
+     end
 end
