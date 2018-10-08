bb04ab6340b437d3ed56d1b698969ad83c51c049
HIRONAKAMURA <carraro.ariel@gmail.com>
2018-10-08 08:30:52 -0500

    Se agregaron models y controller

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -1,13 +1,2 @@
 class Contacto < ApplicationRecord
-    #id :primary_key 
-    #nombre :string
-    #apellidos :string
-    #telefono :string
-    #correo :string
-    #direccion :string
-    #fecha :datetime
-
-    def self.down
-        drop_table :contacto
-     end
 end
--- db/migrate/20181008132831_create_contactos.rb
+++ db/migrate/20181008132831_create_contactos.rb
@@ -1,6 +1,11 @@
 class CreateContactos < ActiveRecord::Migration[5.2]
   def change
     create_table :contactos do |t|
+      t.string :nombre
+      t.string :apellidos
+      t.string :telefono
+      t.string :correo
+      t.text :direccion
 
       t.timestamps
     end
