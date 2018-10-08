02c18957841b3beb11a5a23ab0793f1233b4398d
HIRONAKAMURA <carraro.ariel@gmail.com>
2018-10-08 08:48:44 -0500

    Se agregaron models y controller

--- app/controllers/contacto_controller.rb
+++ app/controllers/contacto_controller.rb
@@ -1,2 +1,48 @@
 class ContactoController < ApplicationController
+
+    def list
+        @contactos = Contacto.all
+     end
+  
+     def show
+        @contacto = Contacto.find(params[:id])
+     end
+    
+     def new
+        @contacto = Contacto.new
+     end
+  
+     def contacto_params
+        params.require(:contactos).permit(:nombre, :apellidos, :telefono, :correo, :direccion)
+     end
+  
+     def create
+        @contacto = Contacto.new(contacto_params)
+  
+        if @contacto.save
+           redirect_to :action => 'list'
+        end
+     end
+     
+     def edit
+        @contacto = Contacto.find(params[:id])
+     end
+     
+     def contacto_param
+        params.require(:contacto).permit(:nombre, :apellidos, :telefono, :correo, :direccion)
+     end
+     
+     def update
+        @contacto = Contacto.find(params[:id])
+        
+        if @contacto.update_attributes(contacto_param)
+           redirect_to :action => 'show', :id => @contacto
+        end
+     end
+     
+     def delete
+        Contacto.find(params[:id]).destroy
+        redirect_to :action => 'list'
+     end
+     
 end
