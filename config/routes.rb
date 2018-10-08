ed046ccdb571bd2a356ead8d9a198ff756253717
HIRONAKAMURA <carraro.ariel@gmail.com>
2018-10-08 08:51:09 -0500

    Se agregaron models y controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'contacto/list'
+  get 'contacto/new'
+  post 'contacto/create'
+  patch 'contacto/update'
+  get 'contacto/list'
+  get 'contacto/show'
+  get 'contacto/edit'
+  get 'contacto/delete'
+  get 'contacto/update'
 end
